ef8026b6e0f7e57f02c1f1dfc5dad827683cb13f
AYOKUNLE OLOWONIWA <AYOKUNLE.OLOWONIWA@sterling.ng>
2022-09-24 21:01:45 +0100

    code cleanup

---
+++ walks/walks/Data/WalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using walks.Models.Domain;
+
+namespace walks.Data
+{
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
